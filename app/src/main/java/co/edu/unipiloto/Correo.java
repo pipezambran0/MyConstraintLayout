ae62993842a4e3b7ac52c1f875e85d0f4b802cbe
ifzs <pipezambrano123@gmail.com>
2024-03-06 13:16:35 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/Correo.java
@@ -0,0 +1,48 @@
+package co.edu.unipiloto;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Correo extends AppCompatActivity {
+    private EditText mEditTextTo;
+    private EditText mEditTextSubject;
+    private EditText mEditTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.correo);
+
+        mEditTextTo = findViewById(R.id.editTextEmailAddress);
+        mEditTextSubject = findViewById(R.id.editTextAsunto);
+        mEditTextMessage = findViewById(R.id.editTextMensaje);
+
+        Button buttonSend = findViewById(R.id.button6);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail();
+            }
+        });
+    }
+
+    private void sendMail() {
+        String recipientList = mEditTextTo.getText().toString();
+        String[] recipients = recipientList.split(",");
+
+        String subject = mEditTextSubject.getText().toString();
+        String message = mEditTextMessage.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "Choose an email client"));
+    }
+}
---
+++ app/src/main/java/co/edu/unipiloto/MainActivity.java
@@ -0,0 +1,24 @@
+package co.edu.unipiloto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /* Ejercicio 1 - pag 43 */
+        /* setContentView(R.layout.exercise_1); */
+        /* Ejercicio 2 - pag 43 */
+        /* setContentView(R.layout.exercise_2); */
+        /* Ejercicio 3 - pag 43 */
+        /* setContentView(R.layout.exercise_3); */
+        /* Ejercicio 4 - pag 43 */
+        /* setContentView(R.layout.exercise_4); */
+
+        /* Formulario solicitudes */
+        setContentView(R.layout.solicitudes);
+    }
+}
